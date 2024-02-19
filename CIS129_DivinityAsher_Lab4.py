84cdc824600bc322d84855ec2ed6208ebb5603bc
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-02-18 22:16:43 -0700

    1st final draft CIS129_DivinityAsher_Lab4.py
    Finished including all the code from the worksheet onto the data including code for calculating the store bonus and the employee bonus. There is also code for printing the information calculated through this program

--- CIS129_DivinityAsher_Lab4.py
+++ CIS129_DivinityAsher_Lab4.py
@@ -1,7 +1,7 @@
 # Module 4 Lab-4
 # Divinity Asher
 # 02/18/2024
-# This program calculates the bonuses received based on the store's sale amount
+# This program calculates and prints the bonuses received based on the store's sales amount
 
 # Variables
 monthlySales = 0 # monthly sales amount
@@ -11,10 +11,36 @@ salesIncrease = 0 # percent of sales increase
 prompt = "What is your shop's " # beginning snippet of user prompt
 
 # User inputs for monthly sales and increase rates
-monthlySales = float(input(prompt, 'monthly sales?: $'))
+monthlySales = float(input(prompt, 'monthly sale amount?: $'))
 
-# Calculation code for determining store bonus
+# This code determines the store bonus based on monthly sales
 if monthlySales >= 110000:
-  storeamount = 6000
+  storeAmount = 6000
 elif monthlySales >= 100000:
-  storeamount = 5000
+  storeAmount = 5000
+elif monthlySale >= 90000:
+  storeAmount = 4000
+elif monthlySale >= 80000:
+  storeAmount = 3000
+else:
+  storeAmount = 0
+
+# This code calculates the sale increase percentage
+salesIncrease = float(input(prompt, 'sale increase percentage?'))
+salesIncrease = salesIncrease / 100
+
+# This code determines the employee bonus
+if salesIncrease >= .05:
+  empAmount = 75
+elif salesIncrease >= .04:
+  empAmount = 50
+elif salesIncrease >= .03:
+  empAmount = 40
+else:
+  empAmount = 0
+
+# This code prints the determined bonus amount and acknowledges whether the bonus is at its maximum.
+print('The store bonus amount is $', storeAmount)
+print('The employee bonus amount is $', empAmount)
+if (storeAmount == 6000) AND (empAmount == 75):
+  print('Congrats! You have reached the highest bonus amounts possible!')
