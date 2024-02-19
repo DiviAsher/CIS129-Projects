b722c583b515f6a30440ced0ed4c5424b6dafeea
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-02-18 22:37:15 -0700

    Added missing s from monthlySales variables in code

--- CIS129_DivinityAsher_Lab4.py
+++ CIS129_DivinityAsher_Lab4.py
@@ -19,9 +19,9 @@ if monthlySales >= 110000:
   storeAmount = 6000
 elif monthlySales >= 100000:
   storeAmount = 5000
-elif monthlySale >= 90000:
+elif monthlySales >= 90000:
   storeAmount = 4000
-elif monthlySale >= 80000:
+elif monthlySales >= 80000:
   storeAmount = 3000
 else:
   storeAmount = 0
