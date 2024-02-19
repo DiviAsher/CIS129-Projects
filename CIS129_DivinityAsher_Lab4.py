dffbb09334f680e17117fcf18e77fb50f2ea11e2
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-02-18 18:04:09 -0700

    Update 1 CIS129_DivinityAsher_Lab4.py
    Started program, saving to attempt an initial run of variables with first input prompt.

--- CIS129_DivinityAsher_Lab4.py
+++ CIS129_DivinityAsher_Lab4.py
@@ -1,1 +1,14 @@
+# Module 4 Lab-4
+# Divinity Asher
+# 02/18/2024
+# This program calculates the bonuses received based on the store's sale amount
 
+# Variables
+monthlySales = float(input(prompt, 'monthly sales?: $')) # monthly sales amount
+storeAmount = 0 # store bonus amount
+empAmount = 0 # employee bonus amount
+salesIncrease =  # percent of sales increase
+prompt = string("What is your shop's ") # beginning snippet of user prompt
+
+# User inputs for monthly sales and increase rates
+input(monthlySales)
