ae3760771b3aad5a973615c2153de3c1d7f7dc89
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-02-29 19:45:07 -0700

    Create CIS129_DivinityAsher_Lab5.py

---
+++ CIS129_DivinityAsher_Lab5.py
@@ -0,0 +1,12 @@
+# Module 5 Lab - Bottle return program
+# Divinity Asher
+# 02/29/2024
+# This program receives data about the amount of bottles returned in a week and calculates the total payout after the week
+
+# Initialize variables for enabling the loop statement, tracked bottles, and tracked payout 
+
+
+# Process the input of each day of bottles collected 
+
+
+# Terminate
