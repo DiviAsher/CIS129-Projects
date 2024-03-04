23602ceb9b0ca0c1e284d8e666f9e45f479cfe80
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-03-04 15:55:14 -0700

    Adjusted positioning of functions to define them before calling them

--- CIS129_DivinityAsher_Lab6.py
+++ CIS129_DivinityAsher_Lab6.py
@@ -4,15 +4,12 @@
 
 totalHotDogs = 0   # Accumulator of hot dog total
 
-totalHotDogs = getTotalHotDogs();   # Calls the function to receive input necessary to calculate the minimimum number of hot dogs needed and assigns it to the accumulator variable.
-showResults();   # Calls the function to show number of hot dogs and buns left over
-
 def getTotalHotDogs():
     """Prompts user to input the amount of attendees attending as well as the amount of hot dogs allowed per attendee"""
     attendees = int(input('What is the maximum number of attendees for the cookout? '))   # Asks the user for the number of attendees for the cookout
     hotDogs = int(input('How many designated hot dogs per attendee? '))   # Asks the user for the amount of hot dogs per attendee
-    Total = attendees * hotDogs
-    return Total
+    totalHotDogs = attendees * hotDogs   
+    return totalHotDogs
 
 def showResults():
     """Calculates the minimum amount of hot dogs and buns needed, the hot dogs and buns remaining, and prints said results"""
@@ -27,3 +24,6 @@ def showResults():
     print('Minimum packages of hot dog buns needed ', minBuns)
     print('Hot dogs remaining ', dogsLeft)
     print('Hot dogs bun remaining ', bunsLeft)
+
+totalHotDogs = getTotalHotDogs();   # Calls the function to receive input necessary to calculate the minimimum number of hot dogs needed and assigns it to the accumulator variable.
+showResults();   # Calls the function to show number of hot dogs and buns left over
