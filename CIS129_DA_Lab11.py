06fcce45c55dce152ef0739b7359f304960bf4d9
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-04-26 14:30:38 -0700

    Update CIS129_DA_Lab11.py

--- CIS129_DA_Lab11.py
+++ CIS129_DA_Lab11.py
@@ -1,17 +1,19 @@
 
-# CIS129 - Module 11 Lab: Grade file storage
+# CIS129 - Module 11 Lab: Class Average Program
 # Divinity Asher
 # 04/26/2024
 # This program allows the user to enter grades into a text file.
 
-# Main function
-def main():
+# Write function
+def writeText():
     print()
     with open('grades.txt', mode='w') as grades:
         gradeInput = grades.write(input("Please enter the grades from the class, use -1 to end the program: "))
     
     while gradeInput != -1:
+        with open('grades.txt', mode='w') as grades:
+            gradeInput = grades.write(input("Enter grade, -1 to end: "))
         
+# Call to write the grades in the text file.
+writeText()
 
-# Call to main
-main()
