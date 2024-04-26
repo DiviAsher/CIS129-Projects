56c3e0f2866482e18681e2ee32ac01c294310e02
DiviAsher <159057653+DiviAsher@users.noreply.github.com>
2024-04-26 14:54:04 -0700

    Update CIS129_DA_Lab11.py

--- CIS129_DA_Lab11.py
+++ CIS129_DA_Lab11.py
@@ -9,12 +9,11 @@ def writeText():
     print()
     with open('grades.txt', mode='w') as grades:
         gradeInput = grades.write(input("Please enter the grades from the class, use -1 to end the program: "))
-        close
     
     while gradeInput != "-1":
         with open('grades.txt', mode='w') as grades:
             gradeInput = grades.write(input("Enter grade, -1 to end: "))
-        return gradeInput
+        grades.close()
         
 # Call to write the grades in the text file.
 writeText()
